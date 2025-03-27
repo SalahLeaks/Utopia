760b98040f44ca0dfbbbf3aaaab321c96da5e41f
SalahAlt <salahtheking2011@gmail.com>
2025-03-28 01:52:55 +0300

    Update DiscordService.cs

--- Clyde/Services/DiscordService.cs
+++ Clyde/Services/DiscordService.cs
@@ -2,65 +2,66 @@ using Discord;
 using Discord.WebSocket;
 using Serilog;
 using Serilog.Events;
+using Apollo.Service;
 
-namespace Apollo.Service
+namespace Apollo.Service;
+
+public static class DiscordService
 {
-    public static class DiscordService
+    private const string Token = "";
+    private const ulong ChannelId = 0;
+
+    private static readonly DiscordSocketClient Client;
+
+    static DiscordService()
     {
-        private const string Token = "";
-        private static readonly DiscordSocketClient Client;
+        Client = new DiscordSocketClient();
+    }
 
-        static DiscordService()
+    public static async Task InitializeAsync()
+    {
+        Client.Log += LogAsync;
+        
+        await Client.LoginAsync(TokenType.Bot, Token);
+        await Client.StartAsync();
+    }
+    
+    /// <summary>
+    /// Sends the exported file (or zip) to the Discord channel.
+    /// </summary>
+    /// <param name="filePath">Absolute path of the file to send.</param>
+    public static async Task SendExportFileAsync(string filePath)
+    {
+        if (await Client.GetChannelAsync(ChannelId) is not IMessageChannel channel)
         {
-            Client = new DiscordSocketClient();
-        }
-
-        public static async Task InitializeAsync()
-        {
-            Client.Log += LogAsync;
-            
-            await Client.LoginAsync(TokenType.Bot, Token);
-            await Client.StartAsync();
+            Log.Error("Unable to find channel");
+            return;
         }
         
-        /// <summary>
-        /// Sends the exported file (or zip) to the specified Discord channel.
-        /// </summary>
-        /// <param name="channelId">The Discord channel ID to send the file to.</param>
-        /// <param name="filePath">Absolute path of the file to send.</param>
-        public static async Task SendExportFileAsync(ulong channelId, string filePath)
+        if (!File.Exists(filePath))
         {
-            if (await Client.GetChannelAsync(channelId) is not IMessageChannel channel)
-            {
-                Log.Error("Unable to find channel");
-                return;
-            }
-            
-            if (!File.Exists(filePath))
-            {
-                Log.Error("File {filePath} does not exist.", filePath);
-                return;
-            }
-            
-            await channel.SendFileAsync(filePath, "Here is your exported file");
-            Log.Information("Sent exported file {filePath} to Discord", filePath);
+            Log.Error("File {filePath} does not exist.", filePath);
+            return;
         }
         
-        private static async Task LogAsync(LogMessage message)
+        await channel.SendFileAsync(filePath, "Here is your exported file");
+        Log.Information("Sent exported file {filePath} to Discord", filePath);
+    }
+    
+    private static async Task LogAsync(LogMessage message)
+    {
+        var severity = message.Severity switch
         {
-            var severity = message.Severity switch
-            {
-                LogSeverity.Critical => LogEventLevel.Fatal,
-                LogSeverity.Error => LogEventLevel.Error,
-                LogSeverity.Warning => LogEventLevel.Warning,
-                LogSeverity.Info => LogEventLevel.Information,
-                LogSeverity.Verbose => LogEventLevel.Verbose,
-                LogSeverity.Debug => LogEventLevel.Debug,
-                _ => LogEventLevel.Information
-            };
-            
-            Log.Write(severity, message.Exception, "[{Source}] {Message}", message.Source, message.Message);
-            await Task.CompletedTask;
-        }
+            LogSeverity.Critical => LogEventLevel.Fatal,
+            LogSeverity.Error => LogEventLevel.Error,
+            LogSeverity.Warning => LogEventLevel.Warning,
+            LogSeverity.Info => LogEventLevel.Information,
+            LogSeverity.Verbose => LogEventLevel.Verbose,
+            LogSeverity.Debug => LogEventLevel.Debug,
+            _ => LogEventLevel.Information
+        };
+        
+        Log.Write(severity, message.Exception, "[{Source}] {Message}", message.Source, message.Message);
+        await Task.CompletedTask;
     }
 }
